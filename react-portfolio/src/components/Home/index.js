b6a108fb3229b1194e041d5e0623483ee548f1fb
Anuka <anukazan@gmail.com>
2022-09-01 01:12:12 -0700

    home page edited

---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,24 @@
+// import LogoTitle from '../../assets/images/logo-a.png'
+import { Link } from 'react-router-dom'
+import './index.scss';
+
+const Home = () => {
+    return(
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1> Howdy! <br/>I'm  
+                {/* <img src={LogoTitle} alt='developer'/>
+                  nuka */}
+
+                  <span className = "pink"> Anuka</span>,
+                <br/>
+                Web Developer & UX Designer
+                </h1>
+                <h2> Full-Stack Developer /  Javascript Expert / Ramen Enthusiast </h2>
+                <Link to="/contact" className='flat-button'> CONTACT ME </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
